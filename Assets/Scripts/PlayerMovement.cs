d7fa91c59b65ffab5381c250f771052e4eae20ea
Bartek Lech <bartoszlech2005@gmail.com>
2020-09-06 20:34:17 +0200

    commit movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, dashForce;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Movement();
+    }
+
+    void Movement() {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(moveX, moveY, 0) * speed * Time.deltaTime);
+    }
+}
