158ba1c6109ed32210846548c7aa8209b9dbb68b
Bartek Lech <bartoszlech2005@gmail.com>
2020-09-06 22:24:17 +0200

    commit proba dashowania - bledy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed, dashForce;
+    public float speed, dashForce, startDashTime;
+    private float dashTime;
+    public bool isDashing;
+    public enum directions {
+        up,
+        down,
+        left,
+        right
+    }
+    public directions direction;
 
     private Rigidbody2D rb;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+        direction = directions.up;
+        isDashing = false;
     }
 
     void Update() {
+        if (isDashing) dashTime -= Time.time;
+        if (dashTime <= 0f) { rb.velocity = Vector2.zero; isDashing = false; }
         Movement();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            dashTime = startDashTime;
+            Dash(); 
+        }
     }
 
     void Movement() {
@@ -21,5 +39,18 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(moveX, moveY, 0) * speed * Time.deltaTime);
+        if (moveX > 0f) direction = directions.right;
+        else if (moveX < 0f) direction = directions.left;
+        if (moveY > 0f) direction = directions.up;
+        else if (moveY < 0f) direction = directions.down;
+    }
+
+    void Dash() {
+        isDashing = true;
+        Debug.Log("dash");
+        if (direction == directions.up) rb.velocity = Vector2.up * dashForce;
+        if (direction == directions.down) rb.velocity = Vector2.down * dashForce;
+        if (direction == directions.right) rb.velocity = Vector2.right * dashForce;
+        if (direction == directions.left) rb.velocity = Vector2.left * dashForce;
     }
 }
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    private PlayerMovement movementScript;
+
+    void Start() {
+        movementScript = GetComponent<PlayerMovement>();
+    }
+
+    void Update() {
+        
+    }
+}
