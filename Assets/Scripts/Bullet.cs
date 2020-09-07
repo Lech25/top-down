6486db01e3d401c6f350b7c60a1ab45fcc152172
Bartek Lech <bartoszlech2005@gmail.com>
2020-09-07 19:29:09 +0200

    commit strzelanie bledy

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    void Update() {
+        rb.AddForce(transform.up * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
     private PlayerMovement movementScript;
+    public GameObject BulletPrefab;
 
     void Start() {
         movementScript = GetComponent<PlayerMovement>();
     }
 
     void Update() {
+        if (Input.GetMouseButtonDown(0)) Shoot();
+    }
+
+    void Shoot() {
+        Vector2 bulletdirection;
+        if (movementScript.direction == PlayerMovement.directions.up) bulletdirection = new Vector2(0, 1f);
+        else if (movementScript.direction == PlayerMovement.directions.down) bulletdirection = new Vector2(0, -1f);
+        else if (movementScript.direction == PlayerMovement.directions.right) bulletdirection = new Vector2(1f, 0);
+        else bulletdirection = new Vector2(-1f, 0);
         
+        Instantiate(BulletPrefab, transform.position, new Quaternion(bulletdirection.x, bulletdirection.y, 0, 0));
     }
 }
